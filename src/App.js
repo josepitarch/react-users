997a1f82fbc5b1bc31f48e1777903df6505a3bbd
Jopimi <josepitarch98@gmail.com>
2021-12-16 16:22:19 +0100

    [FEAT] Add compontents Container, Card and Button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import useFormulario from './hooks/useFormulario'
 import Input from './components/Input'
+import Card from './components/Card'
+import Container from './components/Container'
+import Button from './components/Button'
 
 function App() {
   const [formulario, handleChange] = useFormulario({
     name: '',
-    lastname: ''
+    lastname: '',
+    email: ''
   })
   
   return (
-    <form>
-      <Input
-        label='Nombre'
-        name='name'
-        onChange={ handleChange }
-        value={ formulario.name }
-      />
-      <Input
-        label='Apellido'
-        name='lastname'
-        onChange={ handleChange }
-        value={ formulario.lastname }
-      />
-    </form>
+    <Container>
+      <Card>
+        <form>
+          <Input
+            label='Nombre'
+            name='name'
+            onChange={ handleChange }
+            value={ formulario.name }
+          />
+          <Input
+            label='Apellido'
+            name='lastname'
+            onChange={ handleChange }
+            value={ formulario.lastname }
+          />
+          <Input
+            label='Correo'
+            name='email'
+            onChange={ handleChange }
+            value={ formulario.email }
+          />
+          <Button>Enviar</Button>
+        </form>
+      </Card>
+    </Container>
   );
 }
 
